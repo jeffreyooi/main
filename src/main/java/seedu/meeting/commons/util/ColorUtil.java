f4bed6f31b5ccd7c53525c4a4cec5f217110d393
jeffreyooi <jeffrey950310@gmail.com>
2018-11-10 14:43:09 +0800

    Update color min value limit for each channel

--- src/main/java/seedu/meeting/commons/util/ColorUtil.java
+++ src/main/java/seedu/meeting/commons/util/ColorUtil.java
@@ -15,6 +15,8 @@ public class ColorUtil {
     public static final String PARSE_FAILURE_MESSAGE =
         "String must be in format (r,g,b) and each value should be between 0 to 255";
 
+    private static final int MIN_COLOR_VALUE = 128;
+
     /**
      * Parses color from String. The format of {@code color} must be in "r,g,b" format.
      */
@@ -25,7 +27,8 @@ public class ColorUtil {
         int green = Integer.parseInt(rgbString[1]);
         int blue = Integer.parseInt(rgbString[2]);
 
-        if (red > 255 || red < 0 || green > 255 || green < 0 || blue > 255 || blue < 0) {
+        if (red > 255 || red < MIN_COLOR_VALUE || green > 255 || green < MIN_COLOR_VALUE
+            || blue > 255 || blue < MIN_COLOR_VALUE) {
             throw new IllegalValueException(PARSE_FAILURE_MESSAGE);
         }
 
@@ -37,9 +40,9 @@ public class ColorUtil {
      */
     public static String getRandomColorString() {
         Random random = new Random();
-        int red = random.nextInt(255);
-        int green = random.nextInt(255);
-        int blue = random.nextInt(255);
+        int red = random.nextInt(255 - MIN_COLOR_VALUE + 1) + MIN_COLOR_VALUE;
+        int green = random.nextInt(255 - MIN_COLOR_VALUE + 1) + MIN_COLOR_VALUE;
+        int blue = random.nextInt(255 - MIN_COLOR_VALUE + 1) + MIN_COLOR_VALUE;
         return red + "," + green + "," + blue;
     }
 
@@ -48,7 +51,8 @@ public class ColorUtil {
      * @throws IllegalValueException if any one of the parameters do not lie within the range of 0 and 255
      */
     public static String parseColorString(int red, int green, int blue) throws IllegalValueException {
-        if (red < 0 || red > 255 || green < 0 || green > 255 || blue < 0 || blue > 255) {
+        if (red < MIN_COLOR_VALUE || red > 255 || green < MIN_COLOR_VALUE || green > 255
+            || blue < MIN_COLOR_VALUE || blue > 255) {
             throw new IllegalValueException(PARSE_FAILURE_MESSAGE);
         }
 
